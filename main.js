f2f73f28002230aa30bdcb3810bf2c91d5d7edf7
fogoplayer <fogoplayer@gmail.com>
2018-01-27 23:37:23 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//Register Service Worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('./service-worker.js').then(function() {
+            console.log('Service Worker registered');
+        });
+}
