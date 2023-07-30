4774976986885f5de633b1e5ff1025f0e1259ca1
fogoplayer <zarinloosli@gmail.com>
2023-07-30 09:40:23 -0600

    Remove graffiti fonts from typography

--- src/global-styles/typography.css.mjs
+++ src/global-styles/typography.css.mjs
@@ -1,13 +1,8 @@
 import { css } from "../libs/lit-all@2.7.6.js";
-import linuxLibertine from "./fonts/linux-libertine/family.css.mjs";
 import materialSymbols from "./fonts/material-symbols/family.css.mjs";
-import sedgwickAveDisplay from "./fonts/sedgwick-ave-display/family.css.mjs";
-import spriteGraffiti from "./fonts/sprite-graffiti/family.css.mjs";
-import donGraffiti from "./fonts/don-graffiti/family.css.mjs";
-import blowBrush from "./fonts/blow-brush/family.css.mjs";
 
 export default css`
-  ${linuxLibertine}${materialSymbols}${sedgwickAveDisplay}${spriteGraffiti}${donGraffiti}${blowBrush}
+  ${materialSymbols}
 
   html {
     font-family: var(--sans);
